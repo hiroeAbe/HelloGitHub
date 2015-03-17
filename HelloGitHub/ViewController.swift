d23771246ffd733f1a4b85884f9e64c688fa74d8
阿部弘枝 <abehiroe@abehiroe-no-MacBook-Air.local>
2015-03-17 15:43:24 +0900

    initial commit

---
+++ HelloGitHub/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloGitHub
+//
+//  Created by 阿部弘枝 on 2015/03/17.
+//  Copyright (c) 2015年 阿部弘枝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Hello
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
